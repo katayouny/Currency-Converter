dee5669b8d0dce4fdd43d53e9699aab8e5663d99
katayouny <k.yousefzadeh@gmail.com>
2023-12-11 19:03:01 +0100

    The Currency Convertor v1.1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Brand Currency form -------------
-let brandObjectHistory = [{}];
+let brandObjectHistory = [];
 let eurRateMoving = 0;
 let usdRateMoving = 0;
 let gpyRateMoving = 0;
@@ -21,41 +21,84 @@ function brandCurrency(event) {
     base: baseCurrency,
     date: new Date().toLocaleDateString(),
     rate: {
-      eurRate: eurRate,
-      usdRate: usdRate,
-      gpyRate: gpyRate,
+      eurRate: +eurRate,
+      usdRate: +usdRate,
+      gpyRate: +gpyRate,
     },
   };
-
   brandObjectHistory.push(brandObject);
+
   return brandObject;
 }
-console.log({ brandObjectHistory }); //why the result shows that array 0 is empty. It shows an emty object !!
+console.log(brandObjectHistory);
 
 // Finding the most moving value -------------
-for (let j = 0; j < brandObjectHistory.length; j++) {
-  if (brandObjectHistory["base"] === "eur") {
-    eurRateMoving = eurRateMoving + 1;
-  } else if (brandObjectHistory["base"] === "usd") {
-    usdRateMoving = usdRateMoving + 1;
-  } else if (brandObjectHistory["base"] === "gpy") {
-    gpyRateMoving = gpyRateMoving + 1;
+let ValutaCountsArray = [];
+let MaxValutaNumber;
+
+document
+  .querySelector("finding-the-most-moving-value_button")
+  .addEventListener("click", showTheMostMovingValuta);
+
+function showTheMostMovingValuta() {
+  for (let j = 0; j < brandObjectHistory.length; j++) {
+    if (brandObjectHistory[j].base === "eur") {
+      eurRateMoving++;
+    } else if (brandObjectHistory[j].base === "usd") {
+      usdRateMoving++;
+    } else if (brandObjectHistory[j].base === "gpy") {
+      gpyRateMoving++;
+    }
   }
+
+  ValutaCountsArray.push(eurRateMoving, usdRateMoving, gpyRateMoving);
+
+  if (
+    eurRateMoving !== usdRateMoving &&
+    eurRateMoving !== gpyRateMoving &&
+    usdRateMoving !== gpyRateMoving
+  ) {
+    MaxValuta = Math.max(...ValutaCountsArray);
+
+    if (MaxValutaNumber === ValutaCountsArray[0]) {
+      console.log(
+        `EUR with a count of ${eurRateMoving} has been the most moving valuta`
+      );
+    } else if (MaxValutaNumber === ValutaCountsArray[1]) {
+      console.log(
+        `USD with a count of ${usdRateMoving} has been the most moving valuta`
+      );
+    } else if (MaxValutaNumber === ValutaCountsArray[2]) {
+      console.log(
+        `GPY with a count of ${gpyRateMoving} has been the most moving valuta`
+      );
+    }
+  } else {
+    console.log("Two or more values has the same count");
+  }
+  return MaxValutaNumber;
 }
 
-if (eurRateMoving > usdRateMoving && eurRateMoving > gpyRateMoving) {
-  console.log(
-    `EUR has the most moving rate - ${eurRateMoving} out of ${brandObjectHistory.length}`
-  );
-} else if (usdRateMoving > eurRateMoving && usdRateMoving > gpyRateMoving) {
-  console.log(
-    `USD has the most moving rate - ${usdRateMoving} out of ${brandObjectHistory.length}`
-  );
-} else if (gpyRateMoving > eurRateMoving && gpyRateMoving > usdRateMoving) {
-  console.log(
-    `USD has the most moving rate - ${gpyRateMoving} out of ${brandObjectHistory.length}`
-  );
-}
+/*if (eurRateMoving > usdRateMoving && eurRateMoving > gpyRateMoving) {
+    console.log(
+      `EUR has the most moving rate - ${eurRateMoving} out of ${brandObjectHistory.length}`
+    );
+  } else if (usdRateMoving > eurRateMoving && usdRateMoving > gpyRateMoving) {
+    console.log(
+      `USD has the most moving rate - ${usdRateMoving} out of ${brandObjectHistory.length}`
+    );
+  } else if (gpyRateMoving > eurRateMoving && gpyRateMoving > usdRateMoving) {
+    console.log(
+      `USD has the most moving rate - ${gpyRateMoving} out of ${brandObjectHistory.length}`
+    );
+  } else if (
+    gpyRateMoving === eurRateMoving ||
+    gpyRateMoving === usdRateMoving ||
+    eurRateMoving === gpyRateMoving
+  ) {
+    console.log(`equal`);
+  }
+}*/
 
 // Currency Converter form --------------
 let convertedAmountValue;
@@ -75,55 +118,54 @@ function valutaConverter(event) {
     "valutas-exchange-selction-quote-valuta"
   ).value;
 
-  if (moneyAmount !== 0 && chosenBaseValuta !== chosenQuoteValuta) {
+  if (moneyAmount) {
     for (let i = 0; i < brandObjectHistory.length; i++) {
       if (
-        chosenBaseValuta === "eurRate" &&
-        chosenQuoteValuta === "usdRate" &&
-        brandObjectHistory["base"] === "eurRate"
+        chosenBaseValuta === "eur" &&
+        chosenQuoteValuta === "usd" &&
+        brandObjectHistory[i].base === "eur"
       ) {
-        convertedAmount = moneyAmount * brandObjectHistory["rate"]["usdRate"];
+        convertedAmount = moneyAmount * brandObjectHistory.rate.usdRate; //convertedAmount = moneyAmount * brandObjectHistory["rate"][usdRate];
       } else if (
-        chosenBaseValuta === "eurRate" &&
-        chosenQuoteValuta === "gpyRate" &&
-        brandObjectHistory["base"] === "eurRate"
+        chosenBaseValuta === "eur" &&
+        chosenQuoteValuta === "gpy" &&
+        brandObjectHistory[i].base === "eur" //brandObjectHistory["base"] === "eur"
       ) {
-        convertedAmount = moneyAmount * brandObjectHistory["rate"]["gpyRate"];
+        convertedAmount = moneyAmount * brandObjectHistory.rate.gpyRate; //convertedAmount = moneyAmount * brandObjectHistory["rate"][gpyRate];
       } else if (
-        chosenBaseValuta === "usdRate" &&
-        chosenQuoteValuta === "eurRate" &&
-        brandObjectHistory["base"] === "usdRate"
+        chosenBaseValuta === "usd" &&
+        chosenQuoteValuta === "eur" &&
+        brandObjectHistory[i].base === "usd"
       ) {
-        convertedAmount = moneyAmount * brandObjectHistory["rate"]["eurRate"];
+        convertedAmount = moneyAmount * brandObjectHistory.rate.eurRate;
       } else if (
-        chosenBaseValuta === "usdRate" &&
-        chosenQuoteValuta === "gpyRate" &&
-        brandObjectHistory["base"] === "usdRate"
+        chosenBaseValuta === "usd" &&
+        chosenQuoteValuta === "gpy" &&
+        brandObjectHistory[i].base === "usd"
       ) {
-        convertedAmount = moneyAmount * brandObjectHistory["rate"]["gpyRate"];
+        convertedAmount = moneyAmount * brandObjectHistory.rate.gpyRate;
       } else if (
-        chosenBaseValuta === "gpyRate" &&
-        chosenQuoteValuta === "eurRate" &&
-        brandObjectHistory["base"] === "gpyRate"
+        chosenBaseValuta === "gpy" &&
+        chosenQuoteValuta === "eur" &&
+        brandObjectHistory[i].base === "gpy"
       ) {
-        convertedAmount = moneyAmount * brandObjectHistory["rate"]["eurRate"];
+        convertedAmount = moneyAmount * brandObjectHistory.rate.eurRate;
       } else if (
-        chosenBaseValuta === "gpyRate" &&
-        chosenQuoteValuta === "usdRate" &&
-        brandObjectHistory["base"] === "gpyRate"
+        chosenBaseValuta === "gpy" &&
+        chosenQuoteValuta === "usd" &&
+        brandObjectHistory[i].base === "gpy"
       ) {
-        convertedAmount = moneyAmount * brandObjectHistory["rate"]["usdRate"];
+        convertedAmount = moneyAmount * brandObjectHistory.rate.usdRate;
       } else
         alert(
           `Insert the money amount for conversion. \n Choose to different valutas if you haven't`
         );
     }
+    convertedAmountValue = convertedAmount;
+    document.getElementById("converted-amount").innerText =
+      convertedAmountValue;
+    return convertedAmount;
   }
-  convertedAmountValue = convertedAmount;
-  return convertedAmount;
 }
 
-document.querySelector("converted-amount").value = convertedAmountValue;
-console.log(convertedAmountValue);
-
-// updateing existing currency conversion rates with new rates form----------------
+// updateing existing currency conversion rates with new rates form----------------console.log(convertedAmountValue);
